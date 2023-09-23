255281c17b7a4d83b0562b44c44d867803ad595b
veydoge <veydoge@gmail.com>
2023-09-23 11:05:07 +0300

    Добавил анимации, преследующую камеру туда-сюда

--- movement.cs
+++ movement.cs
@@ -5,11 +5,23 @@ public partial class movement : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+	AnimatedSprite2D animatedSprite;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    public override void _Ready()
+    {
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+    public override void _Process(double delta)
+    {
+        if (Velocity.X == 0)
+		{
+			animatedSprite.Play("idle");
+		}
+    }
 
-	public override void _PhysicsProcess(double delta)
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
@@ -23,17 +35,28 @@ public partial class movement : CharacterBody2D
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("leftmove", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
+			if (velocity.X < 0)
+			{
+				animatedSprite.Play("going_left");
+			}
+			if (velocity.X > 0)
+			{
+				animatedSprite.Play("going_right");
+			}
+	
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		} 
 		Velocity = velocity;
+		// LookAt(GetGlobalMousePosition());
 		MoveAndSlide();
 	}
 }
