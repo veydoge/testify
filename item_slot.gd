c79f7cf10165afbeb4e501bcd80720dc0cac12ba
veydoge <125701786+veydoge@users.noreply.github.com>
2023-10-31 10:54:35 +0300

    Inventory system changed

--- item_slot.gd
+++ item_slot.gd
@@ -1,14 +1,14 @@
 extends Panel
 
 @onready var backgroundSprite: Sprite2D = $background
-@onready var itemsprite: Sprite2D = $CenterContainer/Panel/item2d
+@onready var itemsprite: TextureRect = $CenterContainer/Panel/item2d
 
 func update(item: InventoryItem):
 	if !item:
-		backgroundSprite.frame = 0
+
 		itemsprite.visible = false
 	else:
-		backgroundSprite.frame = 1
+
 		itemsprite.visible = true
 		itemsprite.texture = item.texture
 
