3447ea1be93f1b390bc68c7a3b122683a089e5a7
veydoge <125701786+veydoge@users.noreply.github.com>
2023-12-06 12:36:02 +0300

    Generation changed
    rooms intersection is not allowed anymore

--- Room.gd
+++ Room.gd
@@ -5,8 +5,15 @@ class_name Room
 var position: Vector2i
 var height: int
 var width: int
+var corners : Array[Vector2i]
 
 func _init(position, height, width):
 	self.position = position
 	self.height = height
 	self.width = width
+	
+	corners = []
+	corners.append(position)
+	corners.append(Vector2i(position.x + width, position.y))
+	corners.append(Vector2i(position.x + width, position.y + height))
+	corners.append(Vector2i(position.x, position.y + height))
--- TileMap.gd
+++ TileMap.gd
@@ -2,6 +2,7 @@ extends TileMap
 
 var projectResolution
 var projectTilesRes
+var RoomArray = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,11 +11,11 @@ func _ready():
 	projectTilesRes = Vector2(ceilf(projectResolution.x / 16), ceilf(projectResolution.y / 16))
 	
 	
-	
 	for i in range(projectTilesRes.x):
 		for j in range(projectTilesRes.y):
 			set_cell(0, Vector2(i, j), 6, Vector2(6, 8), 0)	                                                                                                                                                                                                                                                                                                                                                                                                                          
-			
+
+	
 	for i in range(3):
 		make_an_island(randi_range(0, projectTilesRes.x), randi_range(0, projectTilesRes.y))
 			
@@ -26,12 +27,25 @@ func _ready():
 func make_an_island(x, y):
 	var height = randi_range(2, 6)
 	var width = randi_range(2, 6)
-	for i in range(height):
-		for j in range(width	):
-			set_cell(0, Vector2(x + i, y + j), 0, Vector2(0, 0), 0)
+	var newRoom = Room.new(Vector2i(x, y), height, width)
+	for createdRoom in RoomArray:
+		if (room_intersect(createdRoom, newRoom)):
+			return
+	RoomArray.append(newRoom)
+
 	
-
-
+	for i in range(height):
+		for j in range(width):
+			set_cell(0, Vector2(x + i, y + j), 0, Vector2(0, 0), 0)
+			
+	
+func room_intersect(room1: Room, room2: Room):
+	var intersected = false
+	for corner in room1.corners:
+		if (corner.x >= room2.position.x and corner.x <= room2.position.x + room2.width) and (corner.y >= room2.position.y and corner.y <= room2.position.y + room2.height):
+			intersected = true
+			return intersected
+	return intersected
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
