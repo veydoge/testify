434f1ce376aecd9b10e68ef433e2bfbaeee8cb51
sofaackingsad <107792260+sofaackingsad@users.noreply.github.com>
2023-12-25 16:19:58 +0300

    сделан дизайн магазина с предмета, работают переключения предметов в магазе

---
+++ Global.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var gold = 10000
+var items = {
+	
+	0:{
+		"Name":"flask blue",
+		"Des":"sdfgr",
+		"Cost": 10
+	},
+	1:{
+		"Name":"flask green",
+		"Des":"rehe",
+		"Cost": 100
+	},
+	2:{
+		"Name":"flask red",
+		"Des":"jnk",
+		"Cost": 1000
+	},
+	3:{
+		"Name":"flask yellow",
+		"Des":"trjtrjy",
+		"Cost": 1
+	},
+	4:{
+		"Name":"flask purple",
+		"Des":"rtjnyt",
+		"Cost": 10
+	},
+}
--- shop.gd
+++ shop.gd
@@ -1,16 +1,26 @@
 extends CanvasLayer
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+var currentItem = 0
+var select = 0
 
 func _on_closebtn_pressed():
 	get_node("Anim").play("TransOut")
 	get_tree().paused = false
+
+func switchItem(select):
+	for i in range(Global.items.size()):
+		if select == i:
+			currentItem = select
+			get_node("Control/AnimSprite").play(Global.items[currentItem]["Name"])
+			get_node("Control/Name").text = Global.items[currentItem]["Name"]
+			get_node("Control/Des").text = Global.items[currentItem]["Des"]
+			get_node("Control/Des").text += "\n Cost: " + str(Global.items[currentItem]["Cost"])
+	
+func _on_next_pressed():
+	switchItem(currentItem + 1)
+
+func _on_prev_pressed():
+	switchItem(currentItem - 1)
+
+func _on_buy_pressed():
+	pass # Replace with function body.
