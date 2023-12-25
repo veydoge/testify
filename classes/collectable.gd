18d4ad08a05675daf3561b7486a73243739c48a9
hawk0f <sokolovm107@gmail.com>
2023-12-25 16:08:23 +0300

    [beating-trees] Переписал логику поднятия яблок (по нажатию E, инвентарь теперь на TAB)

--- classes/collectable.gd
+++ classes/collectable.gd
@@ -2,6 +2,24 @@ extends Area2D
 
 @export var itemRes: InventoryItem
 
-func collect(inventory: Inventory):
+@onready var inventory = preload("res://assets/inventory/inventory.tres") 
+
+var isPlayerNear = false
+
+func collect():
 	inventory.insert(itemRes)
 	queue_free()
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		isPlayerNear = true
+
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		isPlayerNear = false
+		
+func _input(event):
+	if event.is_action_pressed("take"):
+		if isPlayerNear:
+			collect()
--- player.gd
+++ player.gd
@@ -1,16 +1,11 @@
 extends CharacterBody2D
 const speed = 300
 @onready var animatedSprite : AnimatedSprite2D = $AnimatedSprite2D
-@onready var inventory = preload("res://assets/inventory/inventory.tres") 
-
-
-
 
 func _physics_process(delta):
 	if (velocity.x == 0):
 		animatedSprite.play("idle")
-	
-	
+		
 	var Direction = Input.get_vector("leftmove", "rightmove", "upmove", "downmove")
 	if (Direction != Vector2.ZERO):
 		velocity.x = Direction.x * speed	
@@ -25,15 +20,3 @@ func _physics_process(delta):
 		velocity.x= move_toward(velocity.x, 0, speed)
 		velocity.y= move_toward(velocity.y, 0, speed)
 	move_and_slide()
-		
-	pass
-	
-	
-
-
-
-
-
-func _on_area_2d_area_entered(area: Area2D):
-	if (area.has_method("collect")):
-		area.collect(inventory)
