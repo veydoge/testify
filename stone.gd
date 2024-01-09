3b4dbef4215632cf20bb849f785829f2af9001be
hawk0f <sokolovm107@gmail.com>
2024-01-10 00:05:01 +0300

    [beating_stones] Реализовал уничтожение камней (без анимации)
    Кирка по умолчанию в слоте 2. При нажатии на цифру 2 как бы берется кирка и можно уничтожать камни. При нажатии на цифру 1 (пока что реализовал только нажатия на 1 и 2) кирка сменится на топор и уничтожение камней будет невозможно. Здоровье камня: 3 тычки. Уничтожать камень на кнопку J.

---
+++ stone.gd
@@ -0,0 +1,16 @@
+extends "res://classes/destroyable.gd"
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		isPlayerNear = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		isPlayerNear = false
+
+func _input(event):
+	if event.is_action_pressed("beat"):
+		if isPlayerNear:
+			if inventory.get_current_slot().item != null:
+				if inventory.get_current_slot().item.name == "pickaxe":
+					super.beat()
